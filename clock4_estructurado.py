d78f28265dbc152f24229a70ee80df576de9e83f
Xiglar <amoedofranco7@gmail.com>
2024-10-13 16:32:25 -0300

    Codigo ordenado e imagenes web

--- clock4_estructurado.py
+++ clock4_estructurado.py
@@ -1,16 +1,47 @@
-import tkinter as tk
+'''
+                ### Bienvenida Zoe Xaubet, feliz cumpleaños!!! ###
+
+    Este proyecto surge de la imposibilidad de descargar el reloj digital de tipo flip en tu versión de MacOS
+y tu gusto por los relojes minimalistas de este tipo.
+No conseguí lograr la animación de "flip" debido a que tkinter no es bueno para eso (desde ya te lo advierto
+por si elegís importarlo en otros proyectos) y chatgpt, quien colaboró como desarrollador ayudante, recomendó
+esta librería pero sin ese aviso previo :)
+Espero que te guste y lo uses en tus 2 pcs a pesar de que en la de windows si puedas descargar el profesional.
+Si te es necesario modificar alguna configuración o querés personalizarlo con otros colores, otra fuente u
+otros tamaños, luego de las lineas de importación de librerías, hay un apartado para que lo personalices a tu gusto.
+Gracias por ser la mejor novia del mundo, y que lo disfrutes, felices 21!!!
+'''
+
+import tkinter as tk # pip install tkinter
 from tkinter.font import Font
 from time import strftime
-import requests
-from PIL import Image, ImageTk
+import requests # pip install requests
+from PIL import Image, ImageTk # pip install pillow
 import io
 
+
+### Personalización ###
+
+custom_font = 'Helvetica Neue Condensed Bold' # Default: 'Helvetica Neue Condensed Bold'
+time_size = 350 # Default: 350 (280-420)
+date_size = 30 # Default: 30
+weather_size = 25 # Default: 25
+
+bg_color = 'black' # Default: 'black'
+fg_time_color = '#AAAAAA' # Default: '#AAAAAA'
+fg_date_color = '#A2A2A2' # Default: '#A2A2A2'
+fg_weather_color = '#A2A2A2' # Default: '#A2A2A2'
+
+### /////////////// ###
+
+
+
+# Configuración de la ventana
 window = tk.Tk()
 window.title("Digital Clock")
-window.configure(background='black')
+window.configure(background=bg_color)
 window.attributes('-fullscreen', True)
 
-custom_font = 'Helvetica Neue Condensed Bold'
 
 #Clave API de OpenWeatherMap
 API_KEY = '3cdcc7df8e583545b9990d8d4d4ab456'
@@ -62,7 +93,7 @@ def update_time_and_weather():
     shade_y = label_shade.winfo_y()  # Obtiene la posición y de la hora
 
     # Posiciona la fecha justo debajo de la hora, alineada al comienzo del texto
-    label_day.place(x=shade_x, y=shade_y+370)
+    label_day.place(x=shade_x, y=shade_y+370+time_size-350)
 
     # Actualizar clima
     temperature, description, icon_photo = get_weather_data()
@@ -73,7 +104,7 @@ def update_time_and_weather():
         label_icon.config(image=icon_photo)
         label_icon.image = icon_photo  # Mantener referencia de la imagen
 
-    # Posicionar las etiquetas
+    # Posiciona el clima justo por encima de la hora, alineada al comienzo del texto
     label_icon.place(x=shade_x, y=shade_y-46)
     label_temperature.place(x=shade_x+50, y=shade_y-46)
     label_description.place(x=shade_x+158, y=shade_y-46)
@@ -81,19 +112,20 @@ def update_time_and_weather():
     # Actualiza la hora cada segundo
     label_hour.after(1000, update_time_and_weather)
 
+
 # Estructurar widgets
-label_shade = tk.Canvas(window, width=450, height=350, bg='black', highlightbackground="black")
+label_shade = tk.Canvas(window, width=time_size+100, height=time_size, bg=bg_color, highlightbackground=bg_color)
 
 # Etiquetas para el reloj y la fecha
-label_hour = tk.Label(window, font=Font(family=custom_font, size = 350), background='black', foreground='#AAAAAA')
-label_minute = tk.Label(window, font=Font(family=custom_font, size = 350), background='black', foreground='#AAAAAA')
-label_day = tk.Label(window, font=Font(family=custom_font, size = 30), background='black', foreground='#A2A2A2')
-line = tk.Canvas(window, width=1200, height=3, bg='black', highlightbackground="black")
+label_hour = tk.Label(window, font=Font(family=custom_font, size = time_size), background=bg_color, foreground=fg_time_color)
+label_minute = tk.Label(window, font=Font(family=custom_font, size = time_size), background=bg_color, foreground=fg_time_color)
+label_day = tk.Label(window, font=Font(family=custom_font, size = date_size), background=bg_color, foreground=fg_date_color)
+line = tk.Canvas(window, width=1200, height=3, bg=bg_color, highlightbackground=bg_color)
 
 # Etiquetas para el clima
-label_icon = tk.Label(window, background='black')  # Aquí va el ícono del clima
-label_temperature = tk.Label(window, font=Font(family=custom_font, size=25), background='black', foreground='#A2A2A2')
-label_description = tk.Label(window, font=Font(family=custom_font, size=25), background='black', foreground='#A2A2A2')
+label_icon = tk.Label(window, background=bg_color)  # Acá va el ícono del clima
+label_temperature = tk.Label(window, font=Font(family=custom_font, size = weather_size), background=bg_color, foreground=fg_weather_color)
+label_description = tk.Label(window, font=Font(family=custom_font, size = weather_size), background=bg_color, foreground=fg_weather_color)
 
 
 # Posicionar la hora en el centro
